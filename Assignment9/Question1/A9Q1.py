eeba2df787128e7e949b5bdaef65492679c3e802
Ryan <rcond002@odu.edu>
2016-04-20 13:51:10 -0400

    PDF and Question 9 Files

---
+++ Assignment9/Question1/A9Q1.py
@@ -0,0 +1,71 @@
+import feedparser
+import re
+from bs4 import BeautifulSoup 
+import urllib.request 
+ 
+
+# Returns title and dictionary of word counts for an RSS feed
+def getwordcounts(url):
+  # Parse the feed
+  d=feedparser.parse(url)
+  wc={}
+
+  # Loop over all the entries
+  count = 0
+  print(len(d.entries))
+  for e in d.entries:
+    if 'summary' in e: summary=e.summary
+    else: summary=e.description
+
+    # Extract a list of words
+    words=getwords(e.title+' '+summary)
+    for word in words:
+      wc.setdefault(word,0)
+      wc[word]+=1
+  return d.feed.title,wc
+
+def getwords(html):
+  # Remove all the HTML tags
+  txt=re.compile(r'<[^>]+>').sub('',html)
+
+  # Split words by all non-alpha characters
+  words=re.compile(r'[^A-Z^a-z]+').split(txt)
+
+  # Convert to lowercase
+  return [word.lower() for word in words if word!='']
+
+
+apcount={}
+wordcounts={}
+filename1 = r"C:\Users\Ryan\Documents\WebScience\Assignment9\Question1\Blog.txt"
+feedlist=[]
+for line in open(filename1):
+	feedlist.append(line)
+	
+
+for feedurl in feedlist:
+  try:
+    title,wc=getwordcounts(feedurl)
+    wordcounts[title]=wc
+    for word,count in wc.items():
+      apcount.setdefault(word,0)
+      if count>1:
+        apcount[word]+=1
+  except:
+    print('Failed to parse feed %s' % feedurl)
+print(len(apcount))
+
+wordlist=[]
+#get 500 words in the range
+for w,bc in apcount.items():
+  frac=float(bc)/len(feedlist)
+  if len(wordlist) <= 99:
+    #if frac>0.1 and frac<0.9:
+    wordlist.append(w)   
+print(len(wordlist))
+
+filename2 = r"C:\Users\Ryan\Documents\WebScience\Assignment9\Question1\RawRSS.txt"
+out=open(filename2,'w')
+response = urllib.request.urlopen(feedlist[0])
+soup = BeautifulSoup(response, 'html.parser')
+out.write(str(soup.encode("utf-8")))
